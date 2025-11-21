56f67ed47e34c99b140b498af097cd4a24750108
admin <admin@admins-MacBook-Air.local>
2025-11-21 15:04:54 +0000

    adding data to tables

--- db/data/schema/users.ts
+++ db/data/schema/users.ts
@@ -1,13 +1,9 @@
-const { pgTable, interger } = require('drizzle-orm/pg-core');
+import { pgTable, integer, varchar, timestamp } from 'drizzle-orm/pg-core';
 
-const users = () => {};
-
-const { pgTable, integer, varchar, timestamp } = require('drizzle-orm/pg-core');
-
- const users = pgTable('users', {
+const users = pgTable('users', {
   user_id: integer().primaryKey().notNull(),
   username: varchar().notNull(),
   created_on: timestamp().defaultNow().notNull()
 });
 
-module.exports = users
+module.exports = users;
