67da0c1de68943f01382e5e51aca7600f562ab09
admin <admin@admins-MacBook-Air.local>
2025-11-24 15:19:36 +0000

    add seed.test.ts

--- __tests__/seed.test.ts
+++ __tests__/seed.test.ts
@@ -1,20 +1,35 @@
-import { beforeAll, afterAll, describe, test, expect } from 'vitest'
+import { beforeAll, afterAll, describe, test, expect } from 'vitest';
 import db from '../db/connection';
-import { seed } from '../db/seed';
-import { usersTable } from '../db/data/schema';
+import { users } from '../db/data/schema/users';
+import * as matchers from 'jest-extended';
+import seedTable from '../db/seed/seed';
+import { sql } from 'drizzle-orm';
+import dropTable from '../db/seed/drop';
 
-beforeAll(() => seed());
-afterAll(() => db.$client.end());
+expect.extend(matchers);
+
+beforeAll(() => seedTable());
+afterAll(() => dropTable());
 
 describe('Seed users table', () => {
-    test('should check if users table has the correct fields and values', () => {
-        return db.select().from(usersTable).then((users) => {
-            users.forEach((user) => {
-                expect(user).toHaveProperty('user_id');
-                expect(user).toHaveProperty('username');
-                expect(user).toHaveProperty('created_on');
-            })
-        })
-    });
+  test('should check if users table has the correct fields and values', () => {
+    return db
+      .select()
+      .from(users)
+      .then(users => {
+        users.forEach(user => {
+          expect(user).toHaveProperty('user_id');
+          expect(user).toHaveProperty('username');
+          expect(user).toHaveProperty('created_on');
+        });
+      });
+  });
+  test('Should check that the table containts 10 users', () => {
+    return db
+      .select()
+      .from(users)
+      .then(users => {
+        expect(users.length).toBe(10);
+      });
+  });
 });
-
