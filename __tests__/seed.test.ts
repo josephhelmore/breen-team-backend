9313b501384ed1fb11cdd0926c899348d01ae17d
admin <admin@admins-MacBook-Air.local>
2025-11-25 14:09:34 +0000

    add seed tests

--- __tests__/seed.test.ts
+++ __tests__/seed.test.ts
@@ -1,46 +1,105 @@
 import { beforeAll, afterAll, describe, test, expect } from 'vitest';
-import db from '../db/connection';
 import { users } from '../db/data/schema/users';
-import * as matchers from 'jest-extended';
-import seedTable from '../db/seed/seed';
-import { sql } from 'drizzle-orm';
-import dropTable from '../db/seed/drop';
 import { scores } from '../db/data/schema/scores';
+import { games } from '../db/data/schema/games';
+
+import matchers from 'jest-extended';
+import db from '../db/connection';
+import seedTable from '../db/seed/seed';
+import dropTable from '../db/seed/drop';
 
 expect.extend(matchers);
 
 beforeAll(() => seedTable());
 afterAll(() => dropTable());
 
-describe('Seed users table', () => {
-  test('should check if users table has the correct fields and values', () => {
+describe('Data Insertion for user table', () => {
+  test('should check if users table has the correct columns and data types', () => {
+    return db
+      .select()
+      .from(users)
+      .then(users => {
+        expect(users).toHaveLength(10);
+        users.forEach(user => {
+          expect(user).toHaveProperty('user_id');
+          expect(user).toHaveProperty('username');
+          expect(user).toHaveProperty('created_on');
+          expect(typeof user.user_id).toBe('number');
+          expect(typeof user.username).toBe('string');
+          expect(typeof user.created_on).toBe('object');
+        });
+      });
+  });
+  test('Should check the users table has the correct data-types', () => {
     return db
       .select()
       .from(users)
       .then(users => {
         users.forEach(user => {
-          expect(user).toHaveProperty('user_id');
-          expect(user).toHaveProperty('username');
-          expect(user).toHaveProperty('created_on');
+          expect(typeof user.user_id).toBe('number');
+          expect(typeof user.username).toBe('string');
+          expect(typeof user.created_on).toBe('object');
         });
       });
   });
-  test('Should check that the table containts 10 users', () => {
-    return db
-      .select()
-      .from(users)
-      .then(users => {
-        expect(users.length).toBe(10);
-      });
-  });
 });
-  test('Should check that the scores table has a column referenced to the users table', () => {
+
+describe('Data insertion for scores table', () => {
+  test('Should check that the scores table has the correct columns', () => {
     return db
       .select()
       .from(scores)
       .then(scores => {
-        scores.forEach( score => {
+        expect(scores).toHaveLength(10);
+        scores.forEach(score => {
+          expect(score).toHaveProperty('score_id');
+          expect(score).toHaveProperty('score');
+          expect(score).toHaveProperty('user_id');
           expect(score).toHaveProperty('username');
+          expect(score).toHaveProperty('game_id');
+          expect(score).toHaveProperty('created_on');
         });
       });
   });
+  test('Should check the scores table has the correct data-types', () => {
+    return db
+      .select()
+      .from(scores)
+      .then(scores => {
+        scores.forEach(score => {
+          expect(typeof score.score_id).toBe('number');
+          expect(typeof score.score).toBe('number');
+          expect(typeof score.user_id).toBe('number');
+          expect(typeof score.username).toBe('string');
+          expect(typeof score.game_id).toBe('number');
+          expect(typeof score.created_on).toBe('object');
+        });
+      });
+  });
+});
+
+describe('Data insertion into the game table', () => {
+  test('Should check that the game table has the correct columns', () => {
+    return db
+      .select()
+      .from(games)
+      .then(games => {
+        expect(games).toHaveLength(10);
+        games.forEach(game => {
+          expect(game).toHaveProperty('game_id');
+          expect(game).toHaveProperty('name');
+        });
+      });
+  });
+  test('Should check the game table to have the correct data-types', () => {
+    return db
+      .select()
+      .from(games)
+      .then(games => {
+        games.forEach(game => {
+          expect(typeof game.game_id).toBe('number');
+          expect(typeof game.name).toBe('string');
+        });
+      });
+  });
+});
