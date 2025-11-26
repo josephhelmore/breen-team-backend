c7091a44f466a7d5a1663175cd97520510511a36
admin <admin@admins-MacBook-Air.local>
2025-11-26 12:15:16 +0000

    deployment

--- models/read-model.ts
+++ models/read-model.ts
@@ -3,6 +3,7 @@ import db from '../db/connection';
 import { users } from '../db/data/schema/users';
 import { scores } from '../db/data/schema/scores';
 import { desc } from 'drizzle-orm';
+import { Index } from 'drizzle-orm/gel-core';
 
 export const readUsers = async () => {
   return await db.select().from(users);
@@ -16,15 +17,23 @@ export const readUser = async (user_id: number) => {
 };
 
 export const readScores = async (page: number) => {
+  type score = {
+    score_id: number;
+    score: number;
+    user_id: number;
+    username: string;
+    game_id: number;
+    created_on: Date;
+  };
   const limit = 10;
 
   const dbScores = await db.select().from(scores).orderBy(desc(scores.score));
 
   const paginatedScores = dbScores.reduce(
-    (acc, cur) => {
+    (acc: { [key: number]: score[] }, cur) => {
       const currentPage = Object.keys(acc).length;
-      if (acc[currentPage].length < limit) {
-        acc[currentPage].push(cur);
+      if (acc[currentPage]!.length < limit) {
+        acc[currentPage]!.push(cur);
       } else {
         acc[currentPage + 1] = [cur];
       }
