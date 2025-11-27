710a9e7f3ff1de0189b64288409857ba5de49fd5
Ricky <tcchan71@gmail.com>
2025-11-27 14:44:23 +0000

    fixed timeout

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', () => express.static('public'));
+app.use('/api', express.static('public'));
 
 app.get('/', (req, res) => {
   res.redirect('/api');
