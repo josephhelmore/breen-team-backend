1839aae91c8825030b90f39c1d7304f036beefe0
Ricky <tcchan71@gmail.com>
2025-11-29 11:21:32 +0000

    created more test data

--- src/db/data/test/scoresData.ts
+++ src/db/data/test/scoresData.ts
@@ -22,6 +22,132 @@ const scoresData = [
     user_id: 1,
     username: 'testUser1',
     game_id: 1
+  },
+  {
+    score: 500,
+    user_id: 2,
+    username: 'testUser2',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 3,
+    username: 'testUser3',
+    game_id: 1
+  },
+  {
+    score: 100,
+    user_id: 3,
+    username: 'testUser3',
+    game_id: 1
+  },
+  {
+    score: 700,
+    user_id: 4,
+    username: 'testUser4',
+    game_id: 1
+  },
+  {
+    score: 800,
+    user_id: 5,
+    username: 'testUser5',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 2,
+    username: 'testUser2',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
+  },
+  {
+    score: 600,
+    user_id: 1,
+    username: 'testUser1',
+    game_id: 1
   }
 ];
 
