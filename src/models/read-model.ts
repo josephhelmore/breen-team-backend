2f71da70de8ac86d3d0ea9cc004c0a775b73a047
Ricky <tcchan71@gmail.com>
2025-11-30 10:14:07 +0000

    fixed type in models

--- src/models/read-model.ts
+++ src/models/read-model.ts
@@ -12,7 +12,7 @@ export const readUser = async (user_id: number) => {
   return await db.select().from(users).where(eq(users.user_id, user_id));
 };
 
-export const readUserIdByUsername = async (username: string): Promise<{ userid: number }[]> => {
+export const readUserIdByUsername = async (username: string): Promise<{ user_id: number }[]> => {
   return await db
     .select({ user_id: users.user_id })
     .from(users)
