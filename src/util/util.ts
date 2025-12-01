f70a84e2589253537892de3aa3686697fd9c6f0b
Ricky <tcchan71@gmail.com>
2025-12-01 15:54:59 +0000

    fixed ranking for equal scores

--- src/util/util.ts
+++ src/util/util.ts
@@ -2,7 +2,14 @@ import { Score } from '../types/index.js';
 
 export const addRankToScores = (scores: Score[]): Score[] => {
   return scores.map((score: Score, index) => {
-    score.rank = index + 1;
+    if (index > 0 && score.score === scores[index - 1].score) {
+      const { rank } = scores.find(comparedScore => {
+        return comparedScore.score === score.score;
+      });
+
+      score.rank = rank;
+    } else score.rank = index + 1;
+
     return score;
   });
 };
